575befec033e67ee02dd97877263c40f15922a1e
Student John O Sullivan (TL_KCPGD_B_Y4) <t00234079@mymtu.ie>
2025-10-20 10:03:27 +0100

    Signed-off-by: Student John O Sullivan (TL_KCPGD_B_Y4) <t00234079@mymtu.ie>

---
+++ VoxelTexture/Assets/World.cs
@@ -0,0 +1,40 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class World : MonoBehaviour
+{
+    public int worldSize = 5; 
+    private int chunkSize = 16; 
+
+    private Dictionary<Vector3, Chunk> chunks;
+
+    void Start()
+    {
+        chunks = new Dictionary<Vector3, Chunk>();
+
+        GenerateWorld();
+    }
+
+    private void GenerateWorld()
+    {
+        for (int x = 0; x < worldSize; x++)
+        {
+            for (int y = 0; y < worldSize; y++)
+            {
+                for (int z = 0; z < worldSize; z++)
+                {
+                    Vector3 chunkPosition = new Vector3(x * chunkSize, y * chunkSize, z * chunkSize);
+                    GameObject newChunkObject = new GameObject($"Chunk_{x}_{y}_{z}");
+                    newChunkObject.transform.position = chunkPosition;
+                    newChunkObject.transform.parent = this.transform;
+
+                    Chunk newChunk = newChunkObject.AddComponent<Chunk>();
+                    newChunk.Initialize(chunkSize);
+                    chunks.Add(chunkPosition, newChunk);
+                }
+            }
+        }
+    }
+
+    // Additional methods for managing chunks, like loading and unloading, can be added here
+}
